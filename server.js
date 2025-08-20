a7420ecd69bbd9472dd30ae38232cd2844ab76e2
logesh kumar <logeshkumar1918@gmail.com>
2025-08-21 02:20:04 +0530

    fix5: fixed db connection and change pgport

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 import express from "express";
 import cors from "cors";             
-import pkg from "pg";
+import pg from "pg";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
 dotenv.config();
-const { Pool } = pkg;
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const pool = new Pool({
+const pool = new pg.Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
+pool.connect((err) => {
+  if (err) {
+    console.error('Connection error', err.stack);
+  } else {
+    console.log('Connected to database');
+  }
 });
 
 
@@ -61,5 +70,5 @@ app.get("*", (_req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PGPORT || 5000;
 app.listen(PORT, () => console.log(`Server running on :${PORT}`));
