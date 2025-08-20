86ffa14a6e43a96ce309ca19841ff0b0a74d7a74
logesh kumar <logeshkumar1918@gmail.com>
2025-08-21 01:15:03 +0530

    fix4: api_url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [notes, setNotes] = useState([]);
 
 
-  const API_URL = import.meta.env.VITE_API_URL || "";
+  const API_URL = ""; //  const API_URL = import.meta.env.VITE_API_URL || "";
 
   useEffect(() => {
     fetch(`${API_URL}/notes`)
